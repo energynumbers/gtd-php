5c111cf27c4965ff360ef42ee3f19d47b03c299b
Andrew <github@londonanalytics.info>
2015-05-06 21:08:18 +0100

    rich-text editor sort-of working, but some tags need attention: there's a closing <br> that shouldn't be there, and the view rendering doesn't like <p>

--- item.php
+++ item.php
@@ -245,7 +245,21 @@ if (empty($values['recur'])) {
 if ($show['header']) include_once 'headerHtml.inc.php';
 gtd_handleEvent(_GTD_ON_DATA,$pagename);
 if ($show['header']) {
-  if ($_SESSION['useLiveEnhancements']) { ?>
+  if ($_SESSION['useLiveEnhancements']) {
+
+    // wysiwyg.js
+
+    ?>
+    <link rel="stylesheet" href='wysiwyg.js/wysiwyg-editor.css'>
+    <link rel="stylesheet" href='wysiwyg.js/font-awesome.css'>
+    <script src='wysiwyg.js/wysiwyg.js'></script>
+    <script src='wysiwyg.js/wysiwyg-editor.js'></script>
+    <script src='wysiwyg.js/wysiwyg-editor.js'></script>
+    <?php
+    
+    // end of wysiwg.js
+    
+    ?>
     <script type="text/javascript">
     /* <![CDATA[ */
     $(document).ready(function() {
@@ -263,6 +277,249 @@ if ($show['header']) {
             <?php echo "$pid\n,\n$ptitle\n,\n$partt\n,\"{$values['ptype']}\" \n"; ?>
         );
         GTD.parentselect.refinesearch('<?php echo $values['ptype']; ?>');
+      <?php
+      }
+      
+      if ($_SESSION['useLiveEnhancements']) { ?>
+        // wysiwg.js
+      
+        $('textarea').each( function(index, element) {
+            $(element).wysiwyg({
+                classes: 'some-more-classes',
+                // 'selection'|'top'|'top-selection'|'bottom'|'bottom-selection'
+                toolbar: 'top-selection',
+                buttons: {
+                    insertimage: {
+                        title: 'Insert image',
+                        image: '\uf030', 
+                        //showstatic: true,    // wanted on the toolbar
+                        showselection: index == 2 ? true : false    // wanted on selection
+                    },
+                    insertvideo: {
+                        title: 'Insert video',
+                        image: '\uf03d',
+                        //showstatic: true,    // wanted on the toolbar
+                        showselection: index == 2 ? true : false    // wanted on selection
+                    },
+                    insertlink: {
+                        title: 'Insert link',
+                        image: '\uf08e'
+                    },
+                    // Header plugin
+                    header: index != 1 ? false : {
+                        title: 'Header',
+                        image: '\uf1dc', 
+                        popup: function( $popup, $button ) {
+                                var list_headers = {
+                                        // Name : Font
+                                        'Header 1' : '<h1>',
+                                        'Header 2' : '<h2>',
+                                        'Header 3' : '<h3>',
+                                        'Header 4' : '<h4>',
+                                        'Header 5' : '<h5>',
+                                        'Header 6' : '<h6>',
+                                        'Code'     : '<pre>'
+                                    };
+                                var $list = $('<div/>').addClass('wysiwyg-plugin-list')
+                                                       .attr('unselectable','on');
+                                $.each( list_headers, function( name, format ) {
+                                    var $link = $('<a/>').attr('href','#')
+                                                         .css( 'font-family', format )
+                                                         .html( name )
+                                                         .click(function(event) {
+                                                            $(element).wysiwyg('shell').format(format).closePopup();
+                                                            // prevent link-href-#
+                                                            event.stopPropagation();
+                                                            event.preventDefault();
+                                                            return false;
+                                                        });
+                                    $list.append( $link );
+                                });
+                                $popup.append( $list );
+                               }
+                        //showstatic: true,    // wanted on the toolbar
+                        //showselection: false    // wanted on selection
+                    },
+                    bold: {
+                        title: 'Bold (Ctrl+B)',
+                        image: '\uf032', 
+                        hotkey: 'b'
+                    },
+                    italic: {
+                        title: 'Italic (Ctrl+I)',
+                        image: '\uf033', 
+                        hotkey: 'i'
+                    },
+                    underline: {
+                        title: 'Underline (Ctrl+U)',
+                        image: '\uf0cd', 
+                        hotkey: 'u'
+                    },
+                    strikethrough: {
+                        title: 'Strikethrough (Ctrl+S)',
+                        image: '\uf0cc', 
+                        hotkey: 's'
+                    },
+                    forecolor: {
+                        title: 'Text color',
+                        image: '\uf1fc' 
+                    },
+                    highlight: {
+                        title: 'Background color',
+                        image: '\uf043' 
+                    },
+                    alignleft: index != 0 ? false : {
+                        title: 'Left',
+                        image: '\uf036', 
+                        //showstatic: true,    // wanted on the toolbar
+                        showselection: false    // wanted on selection
+                    },
+                    aligncenter: index != 0 ? false : {
+                        title: 'Center',
+                        image: '\uf037', 
+                        //showstatic: true,    // wanted on the toolbar
+                        showselection: false    // wanted on selection
+                    },
+                    alignright: index != 0 ? false : {
+                        title: 'Right',
+                        image: '\uf038', 
+                        //showstatic: true,    // wanted on the toolbar
+                        showselection: false    // wanted on selection
+                    },
+                    alignjustify: index != 0 ? false : {
+                        title: 'Justify',
+                        image: '\uf039', 
+                        //showstatic: true,    // wanted on the toolbar
+                        showselection: false    // wanted on selection
+                    },
+                    subscript: index == 1 ? false : {
+                        title: 'Subscript',
+                        image: '\uf12c', 
+                        //showstatic: true,    // wanted on the toolbar
+                        showselection: true    // wanted on selection
+                    },
+                    superscript: index == 1 ? false : {
+                        title: 'Superscript',
+                        image: '\uf12b', 
+                        //showstatic: true,    // wanted on the toolbar
+                        showselection: true    // wanted on selection
+                    },
+                    indent: index != 0 ? false : {
+                        title: 'Indent',
+                        image: '\uf03c', 
+                        //showstatic: true,    // wanted on the toolbar
+                        showselection: false    // wanted on selection
+                    },
+                    outdent: index != 0 ? false : {
+                        title: 'Outdent',
+                        image: '\uf03b', 
+                        //showstatic: true,    // wanted on the toolbar
+                        showselection: false    // wanted on selection
+                    },
+                    orderedList: index != 0 ? false : {
+                        title: 'Ordered list',
+                        image: '\uf0cb', 
+                        //showstatic: true,    // wanted on the toolbar
+                        showselection: false    // wanted on selection
+                    },
+                    unorderedList: index != 0 ? false : {
+                        title: 'Unordered list',
+                        image: '\uf0ca', 
+                        //showstatic: true,    // wanted on the toolbar
+                        showselection: false    // wanted on selection
+                    },
+                    removeformat: {
+                        title: 'Remove format',
+                        image: '\uf12d' 
+                    }
+                },
+                // Other properties
+                selectImage: 'Click or drop image',
+                placeholderUrl: 'http://www.example.com/',
+                placeholderEmbed: '<embed/>',
+                maxImageSize: [600,200],
+                onImageUpload: function( insert_image ) {
+                  // A bit tricky, because we can't easily upload a file via
+                  // '$.ajax()' on a legacy browser without XMLHttpRequest2.
+                  // You have to submit the form into an '<iframe/>' element.
+                  // Call 'insert_image(url)' as soon as the file is online
+                  // and the URL is available.
+                  // Example server script (written in PHP):
+                  /*
+                  < ?php
+                  $upload = $_FILES['upload-filename'];
+                  // Crucial: Forbid code files
+                  $file_extension = pathinfo( $upload['name'], PATHINFO_EXTENSION );
+                  if( $file_extension != 'jpeg' && $file_extension != 'jpg' && $file_extension != 'png' && $file_extension != 'gif' )
+                      die("Wrong file extension.");
+                  $filename = 'image-'.md5(microtime(true)).'.'.$file_extension;
+                  $filepath = '/path/to/'.$filename;
+                  $serverpath = 'http://domain.com/path/to/'.$filename;
+                  move_uploaded_file( $upload['tmp_name'], $filepath );
+                  echo $serverpath;
+                  // Example client script (without upload-progressbar):
+                  var iframe_name = 'legacy-uploader-' + Math.random().toString(36).substring(2);
+                  $('<iframe>').attr('name',iframe_name)
+                               .load(function() {
+                                  // <iframe> is ready - we will find the URL in the iframe-body
+                                  var iframe = this;
+                                  var iframedoc = iframe.contentDocument ? iframe.contentDocument :
+                                                 (iframe.contentWindow ? iframe.contentWindow.document : iframe.document);
+                                  var iframebody = iframedoc.getElementsByTagName('body')[0];
+                                  var image_url = iframebody.innerHTML;
+                                  insert_image( image_url );
+                                  $(iframe).remove();
+                               })
+                               .appendTo(document.body);
+                  var $input = $(this);
+                  $input.attr('name','upload-filename')
+                        .parents('form')
+                        .attr('action','/script.php') // accessing cross domain <iframes> could be difficult
+                        .attr('method','POST')
+                        .attr('enctype','multipart/form-data')
+                        .attr('target',iframe_name)
+                        .submit();
+                  */
+                },
+                forceImageUpload: false,    // upload images even if File-API is present
+                videoFromUrl: function( url ) {
+                    // Contributions are welcome :-)
+
+                    // youtube - http://stackoverflow.com/questions/3392993/php-regex-to-get-youtube-video-id
+                    var youtube_match = url.match( /^(?:http(?:s)?:\/\/)?(?:[a-z0-9.]+\.)?(?:youtu\.be|youtube\.com)\/(?:(?:watch)?\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\/)([^\?&\"'>]+)/ );
+                    if( youtube_match && youtube_match[1].length == 11 )
+                        return '<iframe src="//www.youtube.com/embed/' + youtube_match[1] + '" width="640" height="360" frameborder="0" allowfullscreen></iframe>';
+
+                    // vimeo - http://embedresponsively.com/
+                    var vimeo_match = url.match( /^(?:http(?:s)?:\/\/)?(?:[a-z0-9.]+\.)?vimeo\.com\/([0-9]+)$/ );
+                    if( vimeo_match )
+                        return '<iframe src="//player.vimeo.com/video/' + vimeo_match[1] + '" width="640" height="360" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
+
+                    // dailymotion - http://embedresponsively.com/
+                    var dailymotion_match = url.match( /^(?:http(?:s)?:\/\/)?(?:[a-z0-9.]+\.)?dailymotion\.com\/video\/([0-9a-z]+)$/ );
+                    if( dailymotion_match )
+                        return '<iframe src="//www.dailymotion.com/embed/video/' + dailymotion_match[1] + '" width="640" height="360" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
+
+                    // undefined -> create '<video/>' tag
+                }
+            })
+            .change(function() {
+                if( typeof console != 'undefined' )
+                    ;//console.log( 'change' );
+            })
+            .focus(function() {
+                if( typeof console != 'undefined' )
+                    ;//console.log( 'focus' );
+            })
+            .blur(function() {
+                if( typeof console != 'undefined' )
+                    ;//console.log( 'blur' );
+            });
+        });
+
+ 
+      
+        // end of wysiwg.js
       <?php } ?>
     });
     /* ]]> */
